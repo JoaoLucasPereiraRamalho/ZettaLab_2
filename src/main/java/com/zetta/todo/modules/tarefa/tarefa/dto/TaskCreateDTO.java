35aabede0d120e8c2878ff373f441a56c6837a71
JoaoLucasPereiraRamalho <jucasramalho@gmail.com>
2026-02-04 17:46:53 -0300

    add: adiciona validações no dto de tarefas

--- src/main/java/com/zetta/todo/modules/tarefa/tarefa/dto/TaskCreateDTO.java
+++ src/main/java/com/zetta/todo/modules/tarefa/tarefa/dto/TaskCreateDTO.java
@@ -1,10 +1,10 @@
 package com.zetta.todo.modules.tarefa.tarefa.dto;
 
+import com.zetta.todo.modules.tarefa.tarefa.domain.TaskPriority;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import com.zetta.todo.modules.tarefa.tarefa.domain.TaskPriority;
-
 import java.time.LocalDateTime;
 
 @Data
@@ -14,10 +14,11 @@ public class TaskCreateDTO {
 
     private String description;
 
-    private LocalDateTime dueDate; // Formato esperado: "2026-02-04T10:00:00"
+    @FutureOrPresent(message = "A data de vencimento não pode ser no passado")
+    private LocalDateTime dueDate;
+
+    private TaskPriority priority;
 
     @NotNull(message = "A categoria é obrigatória")
     private Long categoryId;
-
-    private TaskPriority priority;
 }
