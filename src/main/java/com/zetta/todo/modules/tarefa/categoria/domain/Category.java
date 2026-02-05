ddad8f124c94d6d5f14af956e0fd4e25544c3268
JoaoLucasPereiraRamalho <jucasramalho@gmail.com>
2026-02-04 22:29:52 -0300

    fix: deleta tarefas com id da categoria deletada por cascata

--- src/main/java/com/zetta/todo/modules/tarefa/categoria/domain/Category.java
+++ src/main/java/com/zetta/todo/modules/tarefa/categoria/domain/Category.java
@@ -1,10 +1,13 @@
 package com.zetta.todo.modules.tarefa.categoria.domain;
 
 import com.zetta.todo.modules.usuario.domain.User;
+import com.zetta.todo.modules.tarefa.tarefa.domain.Task;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
+import lombok.ToString;
 
 @Entity
 @Table(name = "categories")
@@ -25,4 +28,8 @@ public class Category {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<Task> tasks;
 }
